a5dce8c9b2821a05ddd9280ded2d33cc5c09117d
victor njuguna <victormoore254@gmail.com>
2018-05-25 16:06:23 +0300

    this commit creates the comment model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,9 @@ class Blog(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(255))
     content = db.Column(db.String)
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    content = db.Column(db.String)
