7716c92fbce0ee7fab1c157232ca1afa38e5e4cc
victor njuguna <victormoore254@gmail.com>
2018-05-25 15:09:57 +0300

    this commit creates the user model and fixes flask migration errors

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True, index = True)
--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = ('DATABASE_URL', 'postgresql+psycopg2://victor:vnju@localhost/blog')
+    SECRET_KEY = 'vnju'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://victor:vnju@localhost/blog')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProdConfig(Config):
-    pass
+    # pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://victor:vnju@localhost/blog')
 
 class DevConfig(Config):
     DEBUG = True
