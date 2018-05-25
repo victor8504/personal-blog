781d4e55f284d0dabf4434c32788c5046f173a4b
victor njuguna <victormoore254@gmail.com>
2018-05-25 16:02:09 +0300

    this commit creates the blog model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,10 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True, index = True)
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.String)
