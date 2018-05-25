f4a30c84698b34111e6c5299930d0cc3a701ad10
victor njuguna <victormoore254@gmail.com>
2018-05-25 11:47:31 +0300

    this commit initialises the bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 login_manager = LoginManager()
@@ -14,5 +16,6 @@ def create_app():
     app = Flask(__name__)
 
     # Initializing flask extensions
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
