9c127592a7e8122bd837e12984a521f1fedb4935
victor njuguna <victormoore254@gmail.com>
2018-05-25 12:09:28 +0300

    this commit sets up both the flask script and the flask migrate extensions

--- manage.py
+++ manage.py
@@ -1,4 +1,15 @@
 from app import create_app
+from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run
