08a8bc9d76f136f4fd30ec02d14abc9075e9b5fd
victor njuguna <victormoore254@gmail.com>
2018-05-25 11:43:29 +0300

    this commit initialises the sqlalchemy and login_manager extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 # create application
 def create_app():
 
     app = Flask(__name__)
+
+    # Initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
