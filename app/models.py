0f65062cdbaf17aa88f92b7978e46fc98abc9f64
victor njuguna <victormoore254@gmail.com>
2018-05-27 09:35:18 +0300

    this commit creates the user  registration route

--- app/models.py
+++ app/models.py
@@ -2,6 +2,10 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Role(db.Model):
     __tablename__ = 'roles'
 
