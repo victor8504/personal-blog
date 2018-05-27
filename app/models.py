7962bd4de238e2d622302d482ee3273f2371afaa
victor njuguna <victormoore254@gmail.com>
2018-05-27 19:49:17 +0300

    this commit defines a defaukt role for users

--- app/models.py
+++ app/models.py
@@ -9,12 +9,16 @@ from flask import current_app
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+# Listing the permission constants
 class Permission:
     FOLLOW = '0*01'
     COMMENT = '0*02'
     WRITE_ARTICLES = '0*04'
     MODERATE_COMMENTS = '0*08'
     ADMINISTER = '0*80'
+
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -26,6 +30,8 @@ class Role(db.Model):
 
     users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
+    
+    # Creation of roles in the database
     @staticmethod
     def insert_roles():
         roles = {
@@ -47,8 +53,10 @@ class Role(db.Model):
             db.session.add(role)
             db.session.commit()
 
+
     def __repr__(self):
         return f'User {self.name}'
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -70,7 +78,9 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
     
+    # User account confirmation
     confirmed = db.Column(db.Boolean, default=False)
 
     def generate_confirmation_token(self, expiration=3600):
@@ -92,6 +102,16 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    # Define a default role for users
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+        if self.role is None:
+            self.role = Role.query.filter_by(default=True).first()
+
+
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))    
 
     blogs = db.relationship("Blog", backref = "user", lazy = "dynamic")
@@ -101,6 +121,7 @@ class User(UserMixin, db.Model):
     # Method to give the models a readable string representation to facilitate debugging and testing
     def __repr__(self):
         return f'User {self.username}'
+
 
 
 class Blog(db.Model):
@@ -113,6 +134,7 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
     comments = db.relationship("Comment", backref = "blog", lazy = "dynamic")
+
 
 
 class Comment(db.Model):
