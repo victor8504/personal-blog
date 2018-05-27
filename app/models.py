b5fa2642d74129a22c40194418ae9b6759b04c8d
victor njuguna <victormoore254@gmail.com>
2018-05-27 07:34:44 +0300

    this commit defines the relationship between the three models

--- app/models.py
+++ app/models.py
@@ -7,6 +7,11 @@ class User(db.Model):
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True, index = True)
 
+    blog = db.relationship("Blog", backref = "user", lazy = "dynamic")
+
+    comment = db.relationship("Comment", backref = "user", lazy = "dynamic")
+
+
 class Blog(db.Model):
     __tablename__ = 'blogs'
 
@@ -14,8 +19,18 @@ class Blog(db.Model):
     title = db.Column(db.String(255))
     content = db.Column(db.String)
 
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    comment = db.relationship("Comment", backref = "blog", lazy = "dynamic")
+
+
 class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer,primary_key = True)
     content = db.Column(db.String)
+
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    
